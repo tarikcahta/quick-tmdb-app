c5987bbdb7bbe86bc43aa36410a7fd11b7026638
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-05-16 20:47:38 +0200

    Create page layout, add router, add context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import HomePage from './components/HomePage/HomePage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ListProvider } from './context/ListContext';
+import CardDetailed from './components/CardDetailed/CardDetailed';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ListProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:listType" element={<HomePage />} />
+            <Route path="/details/:id" element={<CardDetailed />} />
+          </Routes>
+        </div>
+      </ListProvider>
+    </Router>
   );
 }
 
