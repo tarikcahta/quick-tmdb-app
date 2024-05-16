c02c70a8be7167dc210423a4d87b70d656bd182b
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-05-16 20:59:38 +0200

    Create Card component, add routing to component to a detailed page

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+import { CardProps } from '../../types/types';
+import { useList } from '../../hooks/useList';
+import { getMovieDetails, getTVSeriesDetails } from '../../requests';
+
+const Card: React.FC<CardProps> = ({ posterPath, title, id }) => {
+
+  const {setSelectedMediaItem, listType} = useList();
+  
+  const imageUrl = `https://image.tmdb.org/t/p/w300${posterPath}`;
+
+
+  const handleCardClick = async () => {
+    const detailedData = listType === 'tvshows' ? await getTVSeriesDetails(id) : await getMovieDetails(id);
+    setSelectedMediaItem(detailedData);
+  };
+
+  return (
+    <Link onClick={handleCardClick} to={`/details/${id}`}>
+      <div className="tmdb-card">
+        <img className="poster-image" src={imageUrl} alt="" />
+        <p>{title}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
