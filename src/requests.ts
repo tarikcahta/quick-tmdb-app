3f556e0ac936958005bc5f8465e6fe4fa5e2cc71
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-05-16 20:44:18 +0200

    Add details page requests for single movie/tv show

--- src/requests.ts
+++ src/requests.ts
@@ -3,19 +3,18 @@ import {
   Movie,
   TVShow,
   TMDBSearchResponse,
-  Show,
   TMDBResponse,
 } from './types/types';
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY as string;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export const getTVShows = async (): Promise<Show[] | undefined> => {
+export const getTVShows = async (): Promise<TVShow[] | undefined> => {
   try {
     const response: AxiosResponse<TMDBResponse> = await axios.get(
       `${BASE_URL}/tv/popular?api_key=${API_KEY}`
     );
-    return response.data.results as Show[];
+    return response.data.results as TVShow[];
   } catch (error) {
     console.error(error);
   }
@@ -57,5 +56,27 @@ export const searchMovies = async (
   } catch (error) {
     console.error('Error searching movies:', error);
     return undefined;
+  }
+};
+
+export const getMovieDetails = async (movieId: number): Promise<Movie | undefined> => {
+  try {
+    const response: AxiosResponse<Movie> = await axios.get(
+      `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching details for movie ID ${movieId}:`, error);
+  }
+};
+
+export const getTVSeriesDetails = async (seriesId: number): Promise<TVShow | undefined> => {
+  try {
+    const response: AxiosResponse<TVShow> = await axios.get(
+      `${BASE_URL}/tv/${seriesId}?api_key=${API_KEY}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching details for TV series ID ${seriesId}:`, error);
   }
 };
