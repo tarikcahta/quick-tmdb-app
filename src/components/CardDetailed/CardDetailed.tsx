c8d46ddec3c61cf5d018f7d697c5aa7eb9ee7933
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-05-18 18:52:07 +0200

    Add new movie/show data, add back button, do some minor layout fixes

--- src/components/CardDetailed/CardDetailed.tsx
+++ src/components/CardDetailed/CardDetailed.tsx
@@ -1,6 +1,9 @@
 import { useList } from '../../hooks/useList';
 import { TVShow, UnifiedMediaItem } from '../../types/types';
 
+import { useNavigate } from 'react-router-dom';
+import { IoArrowBackCircle } from 'react-icons/io5';
+
 const isTVShow = (item: UnifiedMediaItem): item is TVShow => {
   return 'name' in item;
 };
@@ -8,6 +11,7 @@ const isTVShow = (item: UnifiedMediaItem): item is TVShow => {
 const CardDetailed = () => {
   const { context } = useList();
   const { selectedMediaItem } = context;
+  const navigate = useNavigate();
 
   if (!selectedMediaItem) {
     return <div>No item selected</div>;
@@ -18,25 +22,41 @@ const CardDetailed = () => {
       ? selectedMediaItem.name
       : selectedMediaItem.title
     : '';
+
   const imageUrl = selectedMediaItem
-    ? `https://image.tmdb.org/t/p/w300${selectedMediaItem.poster_path}`
+    ? `https://image.tmdb.org/t/p/w500${selectedMediaItem.poster_path}`
     : '';
 
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="tmdb-card-detailed">
-      {selectedMediaItem.video ? (
-        <iframe
-          className="trailer-video"
-          src={selectedMediaItem.video}
-          title={title}
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      ) : (
-        <img className="poster-image" src={imageUrl} alt={title} />
-      )}
-      <div className="movie-details">
-        <h2>{title}</h2>
+    <div className="tmdb-card-detailed-page">
+      <span onClick={handleBackClick} className="back-button">
+        <IoArrowBackCircle size={70} />
+      </span>
+      <div className="card-detailed">
+        {selectedMediaItem.video ? (
+          <video
+            className="trailer-video"
+            src={selectedMediaItem.video}
+            title={title}
+          ></video>
+        ) : (
+          <img className="poster-image" src={imageUrl} alt={title} />
+        )}
+        <div className="movie-details">
+          <h2>{title}</h2>
+          {selectedMediaItem.genres && (
+            <p>
+              {selectedMediaItem.genres.map((genre) => (
+                <span key={genre.id}>{genre.name}, </span>
+              ))}
+            </p>
+          )}
+          <p>{selectedMediaItem.overview}</p>
+        </div>
       </div>
     </div>
   );
