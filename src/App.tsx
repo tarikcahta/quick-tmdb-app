edda359b4f1408d61eab1b0b48b80a02927f1639
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-05-18 15:51:28 +0200

    Do rerouting for default path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import './styles/App.css';
 import HomePage from './components/HomePage/HomePage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { ListProvider } from './context/ListContext';
 import CardDetailed from './components/CardDetailed/CardDetailed';
 
@@ -10,7 +15,7 @@ function App() {
       <ListProvider>
         <div className="App">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<Navigate to="/tvshows" />} />
             <Route path="/:listType" element={<HomePage />} />
             <Route path="/details/:id" element={<CardDetailed />} />
           </Routes>
