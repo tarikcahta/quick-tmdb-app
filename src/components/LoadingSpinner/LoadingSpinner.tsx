0a0594572fc0a98010742219312a9b8257774f71
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2024-07-11 19:52:48 +0200

    Create spinner component for detailed card component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import '../../styles/LoadingSpinner.css';
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
