aa7487f6699c03d7b38155d5adffd2d7ffd18f49
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2026-01-23 22:55:07 +0100

    minor fix

--- src/components/CommentsList/CommentsList.tsx
+++ src/components/CommentsList/CommentsList.tsx
@@ -2,14 +2,34 @@ import Comment from '../Comment/Comment';
 import { CommentsListProps } from '../../types/types';
 import '../../styles/CommentsList.css';
 
-const CommentsList: React.FC<CommentsListProps> = ({ comments }) => {
+const CommentsList: React.FC<CommentsListProps> = ({
+  comments,
+  currentUserId,
+  editingCommentId,
+  editingValue,
+  onEditStart,
+  onEditChange,
+  onEditSave,
+  onEditCancel,
+  onDelete,
+}) => {
   return (
     <div className="comments-list">
       {comments.map((comment) => (
-        <Comment key={comment.id} author={comment.author} text={comment.text} />
+        <Comment
+          key={comment.id}
+          author={comment.author}
+          text={comment.text}
+          isOwner={Boolean(currentUserId && comment.user_id === currentUserId)}
+          isEditing={editingCommentId === comment.id}
+          editValue={editingCommentId === comment.id ? editingValue : undefined}
+          onEditStart={() => onEditStart?.(comment.id, comment.text)}
+          onEditChange={onEditChange}
+          onEditSave={onEditSave}
+          onEditCancel={onEditCancel}
+          onDelete={() => onDelete?.(comment.id)}
+        />
       ))}
-
-      
     </div>
   );
 };
